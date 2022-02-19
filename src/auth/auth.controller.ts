84e0164efbd27944728bf13c625395e07e948809
Hitiro Tsugawa <hitiro@live.com>
2022-02-19 17:13:57 -0300

    Enviando emails de confirmação e recuperação de senha

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post, Body, ValidationPipe, Get, UseGuards, Req } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, Get, UseGuards, Patch, Param, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { CredentialsDto } from './dto/credentials.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/users/user.entity';
 import { GetUser } from './get-user.decorator';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { UserRole } from 'src/users/user-roles.enum';
 
 @Controller('auth')
 export class AuthController {
@@ -26,6 +28,54 @@ export class AuthController {
   ): Promise<{ token: string }> {
     return await this.authService.signIn(credentiaslsDto);
   }
+
+  @Patch(':token')
+  async confirmEmail(@Param('token') token: string){
+    const user = await this.authService.confirmEmail(token);
+    return{
+      message: 'Email confirmado',
+    };
+  }
+
+  @Post('/send-recover-email')
+  async sendRrecoverPasswordEmail(
+    @Body('email') email: string,
+  ): Promise<{ message: string}>{
+    await this.authService.sendRecoverrPasswordEmail(email);
+    return{
+      message: 'Foi enviado um email com instrições para resetar a sua senha',
+    };
+  }
+
+  @Patch('/reset-password/:token')
+  async resetPassword(
+    @Param('token') token: string,
+    @Body(ValidationPipe) changePasswordDto: ChangePasswordDto,
+  ): Promise<{ message: string }> {
+    await this.authService.resetPassword(token, changePasswordDto);
+
+    return {
+      message: 'Senha alterada com sucesso',
+    };
+  }
+
+  @Patch(':id/change-password')
+  @UseGuards(AuthGuard())
+    async changePassword(
+      @Param('id') id: string,
+      @Body(ValidationPipe) changePasswordDto: ChangePasswordDto,
+      @GetUser() user: User,
+    ) {
+      if (user.role !== UserRole.ADMIN && user.id.toString() !== id)
+      throw new UnauthorizedException(
+        'Você não tem permissão para realizar esta operação',
+      );
+
+      await this.authService.changePassword(id, changePasswordDto);
+      return {
+        message: 'Senha alterada',
+      };
+    }
 
   @Get('/me')
   @UseGuards(AuthGuard())
---
+++ src/configs/mailer.config.ts
@@ -0,0 +1,17 @@
+import { MailerOptions } from'@nestjs-modules/mailer';
+//import { HandlebarsAdapter } from '@nestjs-modules/mailer'; // Sugerido pelo tutorial
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'; // Sugerido pela documentação
+import * as path from 'path';
+
+export const mailerConfig: MailerOptions = {
+    template: {
+        dir: path.resolve(__dirname, '..', '..', 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+            eextName: '.hbs',
+            layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+        },
+    },
+    transport: `smtps://hitiro.tecnico@gmail.com:kh4j1j4***@smtp.gmail.com`,
+    // transport: `smtps://alguem@gmail.com:senha-super-secreta@smtp.gmail.com`
+}
